94f795d10aed1e1dc2380dd50839d9839408c9b6
saifahmad0622 <163310646+saifahmad0622@users.noreply.github.com>
2024-07-17 17:23:13 +0000

    Datatype & Varaible done

---
+++ 01Day_basics/01_varaible.js
@@ -0,0 +1,18 @@
+const accountId = 14445
+let acoountEmail = "saif@gamil.com"
+var accountPassword = "lko1234"
+accountCity = "Lucknow"
+
+// accountId= 123 -- not Allowed
+console.log(accountId)
+
+acoountEmail = "hello@gmail.com"
+accountPassword = "hello123"
+accountCity = "Mumbai"
+let AcountState;
+
+// console.log(acoountEmail)
+// console.log(accountPassword)
+// console.log(accountCity)
+
+console.table([acoountEmail,accountPassword,accountCity,AcountState])
